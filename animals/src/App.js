b14189a39eeeb077d68dc5c3246a492dc75e01ea
RafaelHernandez <68967694+reh119@users.noreply.github.com>
2024-01-16 13:34:32 -0600

    added array to pick new random animal on click

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,11 +1,26 @@
+import { useState } from "react";
+
+// return random animal/string
+function getRandomAnimal() {
+  const animals = ["bird", "cat", "gator", "dog", "gator", "horse"];
+  //generate random number 0-5
+  return animals[Math.floor(Math.random() * animals.length)]; // dont want decimals
+}
+
 function App() {
+  
+    const [animals, setAnimals] = useState([]);
   // handleclick function using arrow function
-  const handleClick = () => { // 2
-    console.log("Button was clicked");
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimal()]); // c takes all existing elements from animals array, and puts them into a new one. at the end array it puts in a new random animal
+    
   };
   return (
     <div>
-      <button onClick={handleClick}>Add Animal</button> 
+      <button onClick={handleClick}>Add Animal</button>
+      <div> 
+        {animals}
+      </div>
     </div>
   );
 }
@@ -22,7 +37,10 @@ Using Events:
     6.) make sure you pass a reference to the function. (dont call it aka use parenthesis!*) we do have some other alternatives:
         - above when referencing the handleCLick into the button element. we dont use () because we want ot give the button the whole function so the button can call the function in the future when the button is clicked
         - if we use the (), then the function will be envoked right away when the component is rendered. it will call onButtonCLick right away and gives the button the return value, not in the future when its clicked 
-
-
-
+Using State System: (updating something on screen)
+    - Using use state
+        1.) define piece of state with useState function
+        2.) give vaue to useState functiion, this is the default starting value
+        3.) use the state in some way in our component( often in returned jsx)
+        4.)when user does something, update the piece of state. this causes react to rerender the component
 */
