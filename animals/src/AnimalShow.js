1362dc8f12c7b88ccdc22d58c8c4631aa13b6b95
RafaelHernandez <68967694+reh119@users.noreply.github.com>
2024-01-16 14:25:04 -0600

    added heart functionality. pre css added

--- animals/src/AnimalShow.js
+++ animals/src/AnimalShow.js
@@ -1,7 +1,46 @@
-function AnimalShow(){
+import {useState} from 'react'
+import bird from './svg/bird.svg'
+import dog from './svg/dog.svg'
+import cat from './svg/cat.svg'
+import horse from './svg/horse.svg'
+import gator from './svg/gator.svg'
+import heart from './svg/heart.svg'
+import cow from './svg/cow.svg';
+
+const svgMap = { // object where key is identical to value 
+    // bird : bird, etc etc  OR 
+    bird, 
+    cat,
+    cow,
+    horse,
+    gator,
+    dog
+}
+
+
+
+function AnimalShow({type}){ // prop of type that is a lowercase string telling us the name of the animal we want to render
+ 
+    const [clicks, setClicks] = useState(0); 
+    const handleClick = () => {
+        // we want to increase heart size on each click of the animal using a piece of state 
+       
+        setClicks(clicks + 1); 
+    };
     return(
-        <div>Cow</div>
+        <div onClick = {handleClick}>
+            <img alt = 'animal' src = {svgMap[type]}/>
+            <img 
+            alt = 'heart'
+            src ={heart} 
+            style = {{width: 10 + 10 * clicks + 'px'}}
+             /> 
+        </div>
+        
     )
 }
 
 export default AnimalShow;
+// our goal is to show a different animal each time show animal is clicked. we will have a list of different components. the animals are stored inside a string. 
+// we want to transform each element in our array of animals into a component where the type prop is the name of the animal.
+// we can use the .map array function from javascript 
--- animals/src/App.js
+++ animals/src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import AnimalShow from "./AnimalShow";
 // return random animal/string
 function getRandomAnimal() {
   const animals = ["bird", "cat", "gator", "dog", "gator", "horse"];
@@ -13,13 +13,28 @@ function App() {
   // handleclick function using arrow function
   const handleClick = () => {
     setAnimals([...animals, getRandomAnimal()]); // c takes all existing elements from animals array, and puts them into a new one. at the end array it puts in a new random animal
-    
   };
+
+//   const renderedAnimals = [
+//     <AnimalShow type = {animals[0]} />,
+//     <AnimalShow type = {animals[1]} /> ...
+//   ]
+ // OR -> 
+  const renderedAnimals = animals.map((animal,index) => {
+    return <AnimalShow type = {animal} key = {index} />
+  });
+  // .map->  you take all elements out of an array, and one by one, you pass it into a 'transformation function' 
+  // for us, animal like 'cat' is provided as first argument. inside of function we will 'work' or do something to this 'cat' string. 
+  // whatever is returned from this function is put into a new array at the exact same index. , this is reperated for all elements in array. 
+  // once we do this for all elements in og array, the entire map function will return the brand new array we just created / transformed. 
+  // the equivalent of what is above renderedAnimals
+  // frequentley used when building lists in React
+  // 
   return (
     <div>
       <button onClick={handleClick}>Add Animal</button>
       <div> 
-        {animals}
+        {renderedAnimals}
       </div>
     </div>
   );
