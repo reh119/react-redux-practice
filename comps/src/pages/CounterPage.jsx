ad24bb38729dd3d3c8a458987870e0882e8f5e5a
RafaelHernandez <68967694+reh119@users.noreply.github.com>
2024-02-05 19:12:30 -0600

    examples of useReducer, and a counter app to show it

--- comps/src/pages/CounterPage.jsx
+++ comps/src/pages/CounterPage.jsx
@@ -1,36 +1,96 @@
 import Button from "../components/Button";
-import { useState } from "react";
+import { useReducer } from "react";
 import Panel from "../components/Panel";
-// import useCounter from "../hooks/use-counter";
+import {produce} from 'immer'
+//import useCounter from "../hooks/use-counter";
+
+const INCREMENT_COUNT = "increment"; // for reducer function
+const SET_VALUE_TO_ADD = "change_value_to_add";
+const DECREMENT_COUNT = "decrement";
+const ADD_VALUE_TO_COUNT = "add_value_to_count";
+
+const reducer = (state, action) => {
+  // used by calling dispatch
+  switch (action.type) {
+    case INCREMENT_COUNT:
+      return {
+        ...state, // all values from exisiting state object(copy and paste)
+        count: state.count + 1,
+      };
+
+    case SET_VALUE_TO_ADD:
+      return {
+        ...state,
+        valueToAdd: action.payload,
+      };
+    case DECREMENT_COUNT:
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    case ADD_VALUE_TO_COUNT:
+      return {
+        ...state, // whats the point of this?
+        // state object might have an additional property that we dont update here, so we dont want to lose it! so we make sure we copy and paste it to new object(...state)
+        count: state.count + state.valueToAdd,
+        valueToAdd: 0,
+        //
+      };
+
+    default:
+      return state;
+  }
+
+  // return state; //default case
+};
 
 function CounterPage({ initialCount }) {
   // const { count, increment } = useCounter(initialCount); // custom hooks!
-  const [count, setCount] = useState(initialCount);
-  const [valueToAdd, setValueToAdd] = useState(0);
+  // const [count, setCount] = useState(initialCount);
+  // const [valueToAdd, setValueToAdd] = useState(0);
 
+  const [state, dispatch] = useReducer(reducer, {
+    count: initialCount, // initial values for this state
+    valueToAdd: 0,
+  });
+  // when we call useReducer, we will get back state variable which is an object, it will have properties of count and valueToAdd.
   const increment = () => {
-    setCount(count + 1);
+    // setCount(count + 1); // future value of state depends on current value of state(count)
+    dispatch({
+      // calls reducer function
+      type: INCREMENT_COUNT,
+    });
   };
 
   const decrement = () => {
-    setCount(count - 1);
+    // setCount(count - 1); // future value of state depends on current value of state(count)
+    dispatch({
+      type: "decrement",
+    });
   };
 
   const handleChange = (event) => {
-     const value = parseInt(event.target.value) || 0; // turn string to int 
-     setValueToAdd(value)
-  }
+    const value = parseInt(event.target.value) || 0; // turn string to int
+    // setValueToAdd(value);
+
+    dispatch({
+      type: SET_VALUE_TO_ADD, // action object
+      payload: value, // add on optional payload property to set value to add to
+    });
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-
-    setCount(count+valueToAdd)
-    setValueToAdd(0);
-  } 
+    // setCount(count+valueToAdd);
+    // setValueToAdd(0);
+    dispatch({
+      type: ADD_VALUE_TO_COUNT,
+    });
+  };
 
   return (
     <Panel className="m-3">
-      <h1 className="text-lg"> Count is {count} </h1>
+      <h1 className="text-lg"> Count is {state.count} </h1>
       <div className="flex flex-row">
         <Button onClick={increment}>Increment</Button>
         <Button onClick={decrement}>Decrement</Button>
@@ -38,7 +98,7 @@ function CounterPage({ initialCount }) {
       <form onSubmit={handleSubmit}>
         <label>Add a lot!</label>
         <input
-          value={valueToAdd || ""}
+          value={state.valueToAdd || ""}
           onChange={handleChange}
           type="number"
           className="p-1 m-2 bg-gray-50 border border-gray-300"
@@ -80,6 +140,66 @@ Brute Force hook creation:
       changing this state makes component re render 
       useful when you have several different closely related pieces of state 
       useful when future state values depend on the current state 
+        useReducer vs useState (commuity convention)
+          useState -> each piece of state defined as a seperate variable
+          useReducer -> all state for the whole component defined in a single object
+
+
+  
+  state -> state variable(like in useState)
+  dispatch -> function to change state (like in useState), when called, react will find reducer function we define and run it. when executed, 
+  the first argument (state) is gonna be what the current 
+  state of component is that is being maintiend by the that reducer. so it will be an object (count and valueToAdd)
+  action -> second argumnet named action by convention. the value of thsi argument is whatever passed into dispatch. 
+
+  so reducer will run, and whatever we return from this function will be the brand new updated state.
+  our component will then re render, we call useReducer again, and the state object will have updated propertires
+
+  Rules around reducer function:
+    whatever uou return will be your new state 
+    if you return nothing then your state will be undefined 
+    reducer funtion will never use no async/await(rule), no request, no promises, no outside variables(convention/recommended), we want our reducer function to only operate on
+     state and action. 
+Reminder: Like almost everywhere else in React, dont directly modify the state object!!
+
+Note: whenevr we call disptach, how do we know which state to change
+  dispatch()
+    - when we call dispatch, we need to pass along some info to tell the reducer how the state should be updated
+    - billions ways to do this!
+    - the react community has come up with a convention on how to tell the reducer what it needs to do
+      solution:
+        - when we nned to modify state, we will call dispatch and always pass in an 'action' object
+        - the 'action' object will always have a 'type' property that is a string. this helps tell the reducer what state update it needs to make, watch spelling here!!
+        - if we need to communicate some data to the reducer, it will be placed on the 'payload' property of the action object
+        - this is very common community convention, not a requirment. react doesnt treat these action objects any differently. 
+
+      Update my state in new way:
+        1.) add a new constant action type
+        2.) add a call to 'dispatch' 
+        3.) add a new case statement in your reducer
+
+        Note on dispatchers: 
+          Usually makes more sense to stuff more logic into the reducer and keep the dispatches simple
+          less duplicated code if you need to dispatch the same action multiple times 
+          part of the goal of reducers is to have a very specific set of ways that state can be changed 
+
+  Introducing Immer:
+    Library lets you write code to directly mutate code. yes we can break the rule
+      not all react projects wul use immer 
+      you NEED to know how to handle states without it 
+        Reducer with Immer:
+          you can mutate state 
+          do not have to return a new value
+          still return in each case, otherwise you get 'fallthrough' 
+        Normal Reducer
+          no directly changing state
+          must return a new value for use state
+        
+
+
+    
+
+  
 
 
 
